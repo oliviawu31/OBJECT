9b5c1508e1bff8ca73fb31cc455ea47bbb6066c8
oliviawu <oliviawu0301@gmail.com>
2024-11-22 14:40:41 +0800

    interface

--- index.php
+++ index.php
@@ -21,20 +21,22 @@
     // animai=>是動物的總稱，也是抽象的，所以可以拿來當宣告
     // class是一個藍圖 => 需要實例化
     class Animal {
-        // 需要加上封裝才能使用$
-        public $type='animal';
-        public $name='John';
-        public $hair_color='black';
-        public $feet=['front-left','front-right','back-left','back-right'];
+        // 需要加上封裝才能使用
+        protected $type='animal';
+        protected $name='John';
+        protected $hair_color='black';
+        protected $feet=['front-left','front-right','back-left','back-right'];
 
 
         // function=>預設為public
+        // constructor: 用來初始化物件的屬性
         function __construct($type,$name,$hair_color){
             $this->type=$type;
             $this->name=$name;
             $this->hair_color=$hair_color;
         }
 
+        // 方法
         function run(){
             echo $this->name.' is running';
         }
@@ -43,14 +45,15 @@
             echo $this->name.' is running at 20km/h';
         }
 
-
+         // getName 方法，取得動物的名稱
+        // 需要內部回傳時使用
         public function getName(){
             return $this->name;
         }
 
-
+     // setName 方法，用來設定動物的名稱
         public function setName($name){
-                $this->name=$name;
+            $this->name=$name;
         }
 
     }
@@ -72,21 +75,56 @@ echo $cat->run();
 echo $cat->speed();
 // print_r($cat->feet);
 
-$cat->name=('John');
+echo $cat->setName('Mary');
 echo $cat->getName();
 
+?>
+
+<h1>繼承</h1>
+<?php
+
+
+class Cat extends Animal implements Behavior{
+    protected $type='cat';
+    protected $name="Judy";
+    function __construct($hair_color){
+        $this->hair_color=$hair_color;
+    }
+
+    function jump(){
+        echo $this->name . "jumpping 2m";
+    }
+
+
+}
+
+
+Interface Behavior{
+    public function run();
+    public function speed();
+    public function jump();
+}
 
 
 
 
 
+$mycat=new Cat('white');
 
-
-
-
+echo $mycat->getName();
+echo "<br>";
+echo $mycat->run();
+echo "<br>";
+echo $mycat->speed();
+echo "<br>";
+$mycat->setName("judy");
+echo $mycat->getName();
+echo "<br>";
+echo $mycat->jump();
 
 
 
 ?>
+
 </body>
 </html>
