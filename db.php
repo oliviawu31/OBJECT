ab797b200855f0da98d6d648bb55a1561761a48e
oliviawu <oliviawu0301@gmail.com>
2024-11-26 16:14:52 +0800

    完成save()函式,可以執行新增和編輯資料

--- db.php
+++ db.php
@@ -29,6 +29,8 @@ class DB{
 
     // all() =>用來查詢資料表中的所有資料
     // ...$arg =>不定參數，這個方法可以接受不同數量的參數
+   
+   //1.撈出所有資料
     function all(...$arg){
         $sql="SELECT * FROM $this->table ";
 
@@ -48,6 +50,7 @@ class DB{
         return $this->fetchAll($sql);
     }
 
+    // 2.只撈出一筆資料
     function find($id){
         $sql="SELECT * FROM $this->table ";
 
@@ -61,6 +64,37 @@ class DB{
         return $this->fetchOne($sql);
     }
 
+    //3.刪除
+    function del($id){
+        $sql="DELETE FROM $this->table ";
+
+            if(is_array($id)){
+                $where=$this->a2s($id);
+                $sql=$sql . " WHERE ". join(" && ",$where);
+            }else{
+                $sql .=  " WHERE `id`='$id' ";
+            }
+            echo $sql;  
+            return $this->pdo->exec($sql);
+    }
+
+    //4. save
+
+    function save($array){
+        if(isset($array['id'])){
+            //update
+            // update table set `欄位1`='值1',`欄位2`='欄位2' where `id`='值'
+            $set=$this->a2s($array);
+            $sql ="UPDATE $this->table SET ".join(',',$set)." where `id`='{$array['id']}'";
+                
+        }else{
+            //insert
+            $cols=array_keys($array);
+            
+            $sql="INSERT INTO $this->table (`".join("`,`",$cols)."`) VALUES('".join("','",$array)."')";
+            return $this->pdo->exec($sql);
+        }
+    }
 
 
     // 將陣列轉成條件字串陣列
@@ -103,11 +137,16 @@ $DEPT=new DB('dept');
 
 // $dept=$DEPT->q("SELECT * FROM dept");
 
-// 如果是要撈全部資料時
+// 1.如果是要撈全部資料時
 // $dept=$DEPT->all(" Order by `id` DESC ");
-//只要找單一筆資料時
-$dept=$DEPT->find(['code'=>'401']);
+//2.只要找單一筆資料時
+// $dept=$DEPT->find(['code'=>'401']);
+//3.刪除
+// $DEPT->del(2);
+// $DEPT->del(['code'=>'401']);
 
+//如果有id 就無法新增 
+$DEPT->save(['code'=>'504','id'=>'7','name'=>'資訊部']);
 dd($dept);
 
 
